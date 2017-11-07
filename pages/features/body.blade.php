c5de6166372d118901fce4aa5ba3768116fabf2f
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-11-06 23:00:43 -0800

    Dynamic content for features page

--- pages/features/body.blade.php
+++ pages/features/body.blade.php
@@ -6,9 +6,13 @@
             <div class="container text-center">
                 <div class="row justify-content-md-center" style="margin-top:25px;">
                     <div class="col-md-7">
-                        <div >
-                            <h2 class="title" style="color:#fff;text-align:left;"><br>The power of IBM Watson.</h2>
-                            <h5 class="description" style="text-align:left;font-weight:300;color:#fff;">Get deep <b>psychological insights</b> from any web page or text with one click. Simply input a URL and PsychoAnalytics will use IBM Watson's <span style="display:inline-block;"><b>Natural Language Understanding</b></span> to extract opinions, personality types, values, emotions, and more.</h5>
+                        <div id="page-heading">
+                            <feature1
+                                    v-for="item in items"
+                                    v-bind:page="item"
+                                    v-bind:key="item.id"
+                                    v-bind:href="item.slug">
+                            </feature1>
                         </div>
                     </div>
                     <div class="col-md-5">
@@ -26,8 +30,14 @@
                     <div class="col-md-6">
                         <div align="left">
                             <span class="badge badge-default" style="margin-bottom:10px;">Coming Soon</span>
-                            <h2 class="title" style="color:#111;text-align:left;margin-bottom:10px;">The flexibility of MonkeyLearn.</h2>
-                            <h5 class="description" style="text-align:left;font-weight:300;color:#111;">Create your own machine learning powered analytics app. Simply create a <b>custom module on MonkeyLearn</b> and paste the URL in your settings.</h5>
+                            <div id="feature2">
+                                <feature2
+                                        v-for="item in items"
+                                        v-bind:page="item"
+                                        v-bind:key="item.id"
+                                        v-bind:href="item.slug">
+                                </feature2>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -38,12 +48,19 @@
                 <div class="row justify-content-md-center">
                     <div class="col-md-6">
                         <div>
-                            <h2 class="title" style="color:#111;text-align:left;"><br>The precision of Mixpanel.</h2>
-                            <h5 class="description" style="text-align:left;font-weight:300;color:#111;">All the <b>topics, keywords, and sentiments</b> that your visitors find most appealing will show up directly in your Mixpanel dashboard, so you can see how they impact your business's performance.</h5>
+
+                            <div id="feature3">
+                                <feature3
+                                        v-for="item in items"
+                                        v-bind:page="item"
+                                        v-bind:key="item.id"
+                                        v-bind:href="item.slug">
+                                </feature3>
+                            </div>
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <img src="https://mixpanel.com/wp-content/uploads/2016/09/logo-clear.png" alt="Mixpanel" class=" rounded" style="padding-top:40px;" />
+                        <img src="https://mixpanel.com/wp-content/uploads/2016/09/logo-clear.png" alt="Mixpanel" class=" rounded" style="" />
                     </div>
                 </div>
             </div>
@@ -66,11 +83,6 @@
         i = ++i % divs.length;
 
     })();
-</script>
-<!-- Start of ParticleJS -->
-<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-<script>
-    particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#001eff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.93415509907748635,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#00ff3c","opacity":0.90517838682439088,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
 </script>
 
 <div class="section bg-gradient-orange" style="box-shadow:0px 0px 60px rgba(0,0,0,0.1);color:#fff;width:100%;">
---
+++ pages/home/scripts.blade.php
@@ -0,0 +1,60 @@
+<script>
+    var slug = "home";
+
+    Vue.component('feature1', {
+        props: ['page'],
+        template: '<div><h2 style="color:#fff;margin-top:25px;">@{{ page.json.feature1.headline }}</h2><h5 class="description" style="color:#fff;text-align:left;">@{{ page.json.feature1.message }}</h5></div>'
+    });
+
+    var page = null;
+    var feature1 = new Vue({
+        el: '#page-heading',
+        data: {
+            items: null
+        },
+        created: function () {
+            var _this = this;
+            $.getJSON('http://localhost:8000/api/page/' + slug + '/random', function (json) {
+                _this.items = {"items": json};
+                page = {"items": json};
+                console.log(json);
+            });
+        }
+    });
+    
+    Vue.component('feature2', {
+        props: ['page'],
+        template: '<div><h2 style="margin-bottom:5px;">@{{ page.json.feature2.headline }}</h2><h5 class="description" style="text-align:left;color:#000;">@{{ page.json.feature2.message }}</h5></div>'
+    });
+    var feature2 = new Vue({
+        el: '#feature2',
+        data: {
+            items: null
+        },
+        created: function () {
+            var _this = this;
+            $.getJSON('http://localhost:8000/api/page/' + slug + '/random', function (json) {
+                _this.items = {"items": json};
+                console.log(json);
+            });
+        }
+    });
+
+    Vue.component('feature3', {
+        props: ['page'],
+        template: '<div><h2 style="margin-bottom:5px;">@{{ page.json.feature3.headline }}</h2><h5 class="description" style="text-align:left;color:#000;">@{{ page.json.feature3.message }}</h5></div>'
+    });
+    var feature2 = new Vue({
+        el: '#feature3',
+        data: {
+            items: null
+        },
+        created: function () {
+            var _this = this;
+            $.getJSON('http://localhost:8000/api/page/' + slug + '/random', function (json) {
+                _this.items = {"items": json};
+                console.log(json);
+            });
+        }
+    });
+</script>
