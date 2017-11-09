7a3612255d29d1b0d614d1e2828675f587c3a05e
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-11-08 18:55:00 -0800

    Single API call for multiple Vue components

--- pages/features/scripts.blade.php
+++ pages/features/scripts.blade.php
@@ -4,60 +4,30 @@
     var ts = new Date().getTime();
     var data = {_: ts};
 
-
     Vue.component('feature1', {
         props: ['page'],
-        template: '<div><h2 style="color:#fff;margin-top:25px;">@{{ page.json.feature1.headline }}</h2><h5 class="description" style="color:#fff;text-align:left;">@{{ page.json.feature1.message }}</h5></div>'
+        template: '<div><h2 style="margin-top:25px;color:#fff;">@{{ page.json.feature1.headline }}</h2><h5 class="description" style="text-align:left;color:#fff;">@{{ page.json.feature1.message }}</h5></div>'
     });
-
-    var page = null;
-    var feature1 = new Vue({
-        el: '#feature1',
-        data: {
-            items: null
-        },
-        created: function () {
-            var _this = this;
-            $.getJSON('/api/page/' + slug + '/random', data, function (json) {
-                _this.items = {"items": json};
-                page = {"items": json};
-                console.log(json);
-            });
-        }
-    });
-
     Vue.component('feature2', {
         props: ['page'],
         template: '<div><h2 style="margin-bottom:5px;">@{{ page.json.feature2.headline }}</h2><h5 class="description" style="text-align:left;color:#000;">@{{ page.json.feature2.message }}</h5></div>'
     });
-    var feature2 = new Vue({
-        el: '#feature2',
-        data: {
-            items: null
-        },
-        created: function () {
-            var _this = this;
-            $.getJSON('/api/page/' + slug + '/random', data, function (json) {
-                _this.items = {"items": json};
-                console.log(json);
-            });
-        }
-    });
-
     Vue.component('feature3', {
         props: ['page'],
         template: '<div><h2 style="margin-bottom:5px;">@{{ page.json.feature3.headline }}</h2><h5 class="description" style="text-align:left;color:#000;">@{{ page.json.feature3.message }}</h5></div>'
     });
-    var feature2 = new Vue({
-        el: '#feature3',
+    var page = null;
+    var feature1 = new Vue({
+        el: '.wrapper',
         data: {
+            views: ['feature1', 'feature1', 'feature3'],
             items: null
         },
         created: function () {
             var _this = this;
-            $.getJSON('/api/page/' + slug + '/random', data, function (json) {
+            $.getJSON('/api/page/' + slug + '/random', function (json) {
                 _this.items = {"items": json};
-                console.log(json);
+                page = {"items": json};
             });
         }
     });
