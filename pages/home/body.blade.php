4dd134d12794058c9b0eb252fafb255c41aeb633
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-11-08 17:05:55 -0800

    Dynamic heading

--- pages/home/body.blade.php
+++ pages/home/body.blade.php
@@ -6,18 +6,12 @@
             <div class="space-50"></div>
             <div class="container text-center">
                 <div class="row justify-content-md-center"  align="center">
-                    <div class="col-md-12" style="margin-bottom:0px;margin-top:25px;">
-                        <h2 class="title" style="width:100%;color:#fff;">
-                            Psycho-analyze your <div id="content-1">customers.</div><div id="content-2">content.</div><div id="content-3">competitors.</div>
-                        </h2>
-                    </div>
-                    <div class="col-md-12" align="center">
-                       <div id="options">
-                           <?php /* <a href="/marketers" class="btn btn-neutral btn-simple btn-round" style="border:1px solid #fff;">For Marketers</a> */?>
-                           <a class="btn btn-neutral btn-simple btn-round" href="javascript:void(0)" onclick="scrollToMore()">Learn More</a>
-                               <?php /* <a href="/journalists" class="btn btn-neutral btn-simple btn-round" style="border:1px solid #fff;">For Journalists</a>*/?>
-                       </div>
-                    </div>
+                    <heading
+                            v-for="item in items"
+                            v-bind:page="item"
+                            v-bind:key="item.id"
+                            v-bind:href="item.slug">
+                    </heading>
                 </div>
             </div>
         </div>
@@ -96,11 +90,6 @@
         i = ++i % divs.length;
 
     })();
-</script>
-<!-- Start of ParticleJS -->
-<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-<script>
-    particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#001eff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.93415509907748635,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#00ff3c","opacity":0.90517838682439088,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
 </script>
 
 <div class="section bg-gradient-orange" style="box-shadow:0px 0px 60px rgba(0,0,0,0.1);color:#fff;width:100%;">
--- pages/home/scripts.blade.php
+++ pages/home/scripts.blade.php
@@ -1,15 +1,27 @@
 <script>
     var slug = 'home';
 
+    Vue.component('heading', {
+        props: ['page'],
+        template: '<div><div class="col-md-12" style="margin-bottom:0px;margin-top:25px;"> <h2 class="title" style="width:100%;color:#fff;">@{{ page.json.heading.headline }}</h2><a href="javascript:void(0)" onclick="scrollToMore()" class="btn btn-round btn-neutral btn-simple"> @{{ page.json.heading.button }}</a></div></div>'
+    });
     Vue.component('feature1', {
         props: ['page'],
         template: '<div><h2 style="margin-top:25px;">@{{ page.json.feature1.headline }}</h2><h5 class="description" style="text-align:left;color:#000;">@{{ page.json.feature1.message }}</h5></div>'
     });
-
+    Vue.component('feature2', {
+        props: ['page'],
+        template: '<div><h2 style="margin-bottom:5px;">@{{ page.json.feature2.headline }}</h2><h5 class="description" style="text-align:left;color:#000;">@{{ page.json.feature2.message }}</h5></div>'
+    });
+    Vue.component('feature3', {
+        props: ['page'],
+        template: '<div><h2 style="margin-bottom:5px;">@{{ page.json.feature3.headline }}</h2><h5 class="description" style="text-align:left;color:#000;">@{{ page.json.feature3.message }}</h5></div>'
+    });
     var page = null;
     var feature1 = new Vue({
-        el: '#feature1',
+        el: '.wrapper',
         data: {
+            views: ['heading', 'feature1', 'feature1', 'feature3'],
             items: null
         },
         created: function () {
@@ -17,43 +29,6 @@
             $.getJSON('/api/page/' + slug + '/random', function (json) {
                 _this.items = {"items": json};
                 page = {"items": json};
-                console.log(json);
-            });
-        }
-    });
-
-    Vue.component('feature2', {
-        props: ['page'],
-        template: '<div><h2 style="margin-bottom:5px;">@{{ page.json.feature2.headline }}</h2><h5 class="description" style="text-align:left;color:#000;">@{{ page.json.feature2.message }}</h5></div>'
-    });
-    var feature2 = new Vue({
-        el: '#feature2',
-        data: {
-            items: null
-        },
-        created: function () {
-            var _this = this;
-            $.getJSON('/api/page/' + slug + '/random', function (json) {
-                _this.items = {"items": json};
-                console.log(json);
-            });
-        }
-    });
-
-    Vue.component('feature3', {
-        props: ['page'],
-        template: '<div><h2 style="margin-bottom:5px;">@{{ page.json.feature3.headline }}</h2><h5 class="description" style="text-align:left;color:#000;">@{{ page.json.feature3.message }}</h5></div>'
-    });
-    var feature2 = new Vue({
-        el: '#feature3',
-        data: {
-            items: null
-        },
-        created: function () {
-            var _this = this;
-            $.getJSON('/api/page/' + slug + '/random', function (json) {
-                _this.items = {"items": json};
-                console.log(json);
             });
         }
     });
