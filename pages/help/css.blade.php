0b8b4f0f8abee65e63e577b91fb255d2d89cbd5c
huntercarter <jhc@Jamess-MacBook-Pro.local>
2018-01-29 14:56:38 -0800

    Landing page

--- pages/help/css.blade.php
+++
@@ -1,7 +0,0 @@
-<style>
-    .page-header-image{
-        background: #8b69ff;
-        background: -webkit-linear-gradient(to top right, #ff35a4, #350090);
-        background: linear-gradient(to top right, #ff35a4, #350090);
-    }
-</style>
